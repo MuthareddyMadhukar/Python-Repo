fd709d88f676e0dd95e8d0cc31e8cb329813c02b
MuthareddyMadhukar <147469968+MuthareddyMadhukar@users.noreply.github.com>
2024-10-21 19:23:58 +0530

    Uploading code: King Tours .py

---
+++ King Tours .py
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class KT{
+    public static void main(String[] args){
+        int x,y,z;
+        Scanner sc=new Scanner(System.in);
+        x=sc.nextInt();
+        y=sc.nextInt();
+        z=x*5+y*7;
+        System.out.printf("%d",z);
+    }
+}
