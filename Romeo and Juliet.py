ed37e386b78227ee5ed02ee1f4e9ca1541c8709d
MuthareddyMadhukar <147469968+MuthareddyMadhukar@users.noreply.github.com>
2024-10-23 18:43:38 +0530

    Uploading code: Romeo and Juliet.py

---
+++ Romeo and Juliet.py
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class R{
+    public static void main(String[] args){
+        int x,y,z;
+        Scanner sc=new Scanner(System.in);
+        x=sc.nextInt();
+        y=sc.nextInt();
+        z=sc.nextInt();
+        System.out.printf("%d",(((5*x)+(10*y))/z));
+    }
+}
