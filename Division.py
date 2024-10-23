b615c13fc339c68b81840e818db9d36704e4d6e5
MuthareddyMadhukar <147469968+MuthareddyMadhukar@users.noreply.github.com>
2024-10-23 18:55:45 +0530

    Uploading code: Division.py

---
+++ Division.py
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class D{
+    public static void main(String[] args){
+        int m,n;
+        Scanner sc=new Scanner(System.in);
+        m=sc.nextInt();
+        n=sc.nextInt();
+        System.out.printf("%d",m/n);
+    }
+}
