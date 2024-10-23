70317093062b3e84e15018cf14c5b7b4f6cf81c9
MuthareddyMadhukar <147469968+MuthareddyMadhukar@users.noreply.github.com>
2024-10-23 18:48:27 +0530

    Uploading code: Multiplication.py

---
+++ Multiplication.py
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class M{
+    public static void main(String[] args){
+        int x,y;
+        Scanner sc=new Scanner(System.in);
+        x=sc.nextInt();
+        y=sc.nextInt();
+        System.out.printf("%d",x*y);
+    }
+}
