67614ee1a8dd4e287116d1ef313208b25e3a455a
MuthareddyMadhukar <147469968+MuthareddyMadhukar@users.noreply.github.com>
2024-10-23 19:21:42 +0530

    Uploading code: Water Taps.py

---
+++ Water Taps.py
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class W{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int x,y;
+        x=sc.nextInt();
+        y=sc.nextInt();
+        System.out.printf("%d",(x*y)/(x+y));
+    }
+}
